0ae393ee7162c7997cc8bceb33ab928c483bbea1
André Silva <andre.beat@gmail.com>
2018-06-29 16:10:36 +0100

    build rocksdb with cmake

--- rocksdb-sys/build_version.cc
+++
@@ -1,4 +0,0 @@
-#include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:d9c289caa41ab2a992679251ee7a2b9efdf9d71c";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2018-06-13";
-const char* rocksdb_build_compile_date = __DATE__;

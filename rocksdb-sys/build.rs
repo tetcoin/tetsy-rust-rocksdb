fd2db932378604e4a9dae6c9be81cbbb792e198d
André Silva <andre.beat@gmail.com>
2018-07-03 17:48:51 +0100

    build with lz4 support

--- rocksdb-sys/build.rs
+++ rocksdb-sys/build.rs
@@ -1,27 +1,49 @@
+extern crate cc;
 extern crate cmake;
 
 use std::env;
 
+use cc::Build;
 use cmake::Config;
+
+fn build_lz4() {
+    Build::new()
+        .include("lz4/lib")
+        .opt_level(3)
+        .file("lz4/lib/lz4.c")
+        .file("lz4/lib/lz4frame.c")
+        .file("lz4/lib/lz4hc.c")
+        .file("lz4/lib/xxhash.c")
+        .compile("liblz4.a");
+}
 
 fn main() {
     let mut cfg = Config::new("rocksdb");
     cfg.define("CMAKE_VERBOSE_MAKEFILE", "ON")
         .register_dep("SNAPPY")
         .define("WITH_SNAPPY", "ON")
+        .register_dep("LZ4")
+        .define("WITH_LZ4", "ON")
         .build_target("rocksdb");
 
     let snappy = env::var_os("DEP_SNAPPY_INCLUDE").expect("DEP_SNAPPY_INCLUDE is set in snappy.");
+    let lz4 = env::current_dir().unwrap().join("lz4/lib");
+
 
     if cfg!(target_env = "msvc") {
         cfg.env("SNAPPY_INCLUDE", snappy);
+        cfg.env("LZ4_INCLUDE", lz4);
 
         println!("cargo:rustc-link-lib=dylib={}", "rpcrt4");
         println!("cargo:rustc-link-lib=dylib={}", "shlwapi");
     } else {
         cfg.define("SNAPPY_INCLUDE_DIR", snappy)
             .define("SNAPPY_LIBRARIES", "/dev/null");
+        cfg.define("LZ4_INCLUDE_DIR", lz4)
+            .define("LZ4_LIBRARIES", "/dev/null");
     }
+
+    build_lz4();
 
     let out = cfg.build();
 
@@ -38,6 +60,7 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", build.display());
     println!("cargo:rustc-link-lib=static=rocksdb");
     println!("cargo:rustc-link-lib=static=snappy");
+    println!("cargo:rustc-link-lib=static=lz4");
 
     // https://github.com/alexcrichton/cc-rs/blob/ca70fd32c10f8cea805700e944f3a8d1f97d96d4/src/lib.rs#L891
     if cfg!(any(target_os = "macos", target_os = "freebsd", target_os = "openbsd")) {
